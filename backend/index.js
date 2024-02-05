42603656b6498fe8dc25fc3409659e04b80c9d0c
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-02-05 16:04:42 +0000

    adding a createPatient route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {PORT, mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
+import { PatientModel } from './models/patientModel.js';
 
 const app = express();
 
@@ -9,7 +10,41 @@ app.get('/', (request,response) =>{
     return response.status(234).send('Welcome to TaB to Stop');
 });
 
+// Route to save a new patient
 
+app.post('/patients', async (request, response) => {
+    try {
+        if(
+            !request.body.age ||
+            !request.body.date ||
+            !request.body.ec_cell ||
+            !request.body.ec_relationship ||
+            !request.body.ec_work ||
+            !request.body.emergency_contact ||
+            !request.body.gz_meds ||
+            !request.body.gz_peak_flow_max ||
+            !request.body.gz_peak_flow_min ||
+            !request.body.name ||
+            !request.body.pr_peak_flow ||
+            !request.body.provider ||
+            !request.body.provider_phone ||
+            !request.body.rz_meds ||
+            !request.body.rz_peak_flow_min ||
+            !request.body.yz_comment ||
+            !request.body.yz_meds ||
+            !request.body.yz_peak_flow_max ||
+            !request.body.yz_peak_flow_min
+
+        ){
+            return response.status(400).send({
+                message: 'Enter all required fields'
+            });
+        }
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
@@ -20,5 +55,5 @@ mongoose
         });
     })
     .catch((error) => {
-        console.log(error)
+        console.log(error);
     });
