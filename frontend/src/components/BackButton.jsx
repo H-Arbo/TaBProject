70bedb9d8c3e326c0cb68255527319320a70a861
hgfairfax Fairfax <hgfairfax@crimson.ua.edu>
2024-02-08 00:28:14 +0000

    Fix BackButton

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom';
 import {BsArrowLeft} from 'react-icons/bs';
 
-const BackButton = ({destination = '/'}) => {
+const BackButton = ({destination = '/patients/home'}) => {
     return(
         <div className='flex'>
             <Link 
