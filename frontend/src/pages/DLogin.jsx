529cfde95af17f5556bde854ff95dfc06d327694
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-02-11 14:06:20 +0000

    working on login/register

--- frontend/src/pages/DLogin.jsx
+++ frontend/src/pages/DLogin.jsx
@@ -1,19 +1,37 @@
-import React from 'react'
 import BackButton from '../components/BackButton';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 export default function DLogin() {
+
+  const [data, setData] = useState({
+    email: '',
+    password: '',
+  })
+  const docLogin = (e) => {
+    e.preventDefault();
+
+  }
+
   return (
     <div className='p-4'>
-      <BackButton/>
-      
-      <div>Doctor Login</div>
+      <BackButton />
+
+      {/* <div>Doctor Login</div>
           <Link to = '/doctor/home'>
             <button type='submit' className='p-2 bg-sky-300 m-8'>
               Submit
             </button>
-          </Link>
+          </Link> */}
+      <form onSubmit={docLogin}>
+        <label>Email</label>
+        <input type='email' placeholder='Enter email' value={data.email}
+          onChange={(e) => setData({ ...data, email: e.target.value })} />
+        <label>Password</label>
+        <input type='password' placeholder='Enter password' value = {data.password}
+            onChange={(e) => setData({...data, password: e.target.value})} />
+        <button type='submit'>Login</button>
+      </form>
     </div>
   )
 }
--- frontend/src/pages/DRegister.jsx
+++ frontend/src/pages/DRegister.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import BackButton from '../components/BackButton';
 import { useState } from 'react';
 import axios from 'axios';
