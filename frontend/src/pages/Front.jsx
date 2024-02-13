ef822d7ac4386be75cc2e6ba8650ebf5c7ec35fc
aegraves1 Graves <aegraves1@crimson.ua.edu>
2024-02-13 22:42:31 +0000

    Made resources page with a link to Nursing college

--- frontend/src/pages/Front.jsx
+++ frontend/src/pages/Front.jsx
@@ -5,30 +5,42 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
-import Button from '../components/Button';
 // import logo from '../images/asthma_logo.png';
 
 
 const Front = () => {
-    const handleClick = () => {};
-    return (
+    return(
         <div className='p-4'>
             <img src="/images/asthma_logo.png" style={{ width: '460px', height: '70px' }} />
-            <div className='flex justify-between items-center' style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Link to="/patients/home">
-                    <Button onClick={handleClick} color="darkblue">Patient Home</Button>
+            <Link to="/patients/home"
+                className=' bg-sky-800 text-white px-4 py-1 rounded-lg w-fit' style={{ marginLeft: "700px" }}>
+                    <button >
+                    Home
+                    </button>
                 </Link>
-            </div>
-            <div className='flex justify-between items-center' style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Link to="/login">
-                    <Button onClick={handleClick} color="darkblue">Login</Button>
+            
+            <div className='flex justify-between items-center' style={{justifyContent: 'center', alignItems: 'center'}}>
+                <Link to="/login"
+                className=' bg-sky-800 text-white px-20 py-20 rounded-lg w-fit'>
+                    <button>
+                        Login
+                    </button>
                 </Link>
-                <Link to="/register">
-                    <Button onClick={handleClick} color="darkblue">Register</Button>
+                <Link to="/register"
+                className=' bg-sky-800 text-white px-20 py-20 rounded-lg w-fit'>
+                    <button>
+                        Register
+                    </button>
+                </Link>
+                <Link to="/resources"
+                className=' bg-sky-800 text-white px-20 py-20 rounded-lg w-fit'>
+                    <button>
+                        Resources
+                    </button>
                 </Link>
             </div>
         </div>
     )
-}
-
-export default Front
+  }
+  
+  export default Front
