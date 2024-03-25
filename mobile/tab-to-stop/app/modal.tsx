edf6627d95333a77aeb2370230b2ef08bc6f249f
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-03-25 23:41:07 +0000

    Added simple link to website in modal.tsx - used link href from expo-router

--- mobile/tab-to-stop/app/modal.tsx
+++ mobile/tab-to-stop/app/modal.tsx
@@ -1,17 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Platform, StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Link } from "expo-router";
 
 export default function ModalScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Menu</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+      <View
+        style={styles.separator}
+        lightColor="#eee"
+        darkColor="rgba(255,255,255,0.1)"
+      />
 
       {/* Use a light status bar on iOS to account for the black space above the modal */}
-      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
+      <Link href="http://localhost:5173">
+        <Text style={styles.title}>To website</Text>
+      </Link>
     </View>
   );
 }
@@ -19,16 +27,16 @@ export default function ModalScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
-    height: .5,
-    width: '80%',
+    height: 0.5,
+    width: "80%",
   },
 });
