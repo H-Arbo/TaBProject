59e15f06fda09887d2db93634002c9892c4038a6
hgfairfax Fairfax <hgfairfax@crimson.ua.edu>
2024-03-27 22:10:27 +0000

    Fixing resources to show on phone screen

--- frontend/src/pages/Resources.jsx
+++ frontend/src/pages/Resources.jsx
@@ -1,106 +1,94 @@
 import React from 'react';
 import BackButton from "../components/BackButton";
 import { Link } from 'react-router-dom';
-import AAAAI_logo from '/images/AAAAI_logo.png';
-import nursing_logo from '/images/nursing_logo.png';
-import GIA_logo from '/images/GIA_Logo.png';
-import AAFA_logo from '/images/AAFA_logo.png';
-import ACAAI_logo from '/images/ACAAI_logo.png';
-import mayo_logo from '/images/mayo_logo.png';
 
 const Resources = () => {
     return (
         <div className='p-9'>
             <BackButton />
 
-            <h1 className="text-center text-2xl font-bold mb-8 flex flex-wrap">Explore these resources!</h1>
-            <div className='flex grid grid-cols-1 sm:grid-cols-3 gap-6'>
+            <h1 className="justify-center text-center text-2xl font-semibold mb-8 flex flex-wrap">Explore these resources!</h1>
+            <div className='flex grid grid-cols-1 sm:grid-cols-4 gap-6'>
                 <Link to="https://www.aaaai.org/"
-                    className='flex flex-wrap border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
-                    <div className="flex flex-wrap logo-container text-center text-2xl p-2" style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}>
-                        <img src='/images/AAAAI_logo.png' alt="/images/AAAAI_logo.png" />
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/AAAAI_logo.png' alt="/images/AAAAI_logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
 
-                        <div>American Academy of Allergy Asthma and Immunology</div>
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">American Academy of Allergy Asthma and Immunology</div>
                     </div>
                 </Link>
                 <Link to="https://nursing.ua.edu/"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
-                    <div className="logo-container text-center text-2xl p-2" style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}>
-                    <img src='/images/nursing_logo.png' alt="/images/nursing_logo.png" />
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/nursing_logo.png' alt="/images/nursing_logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
 
-                    <div>The University of Alabama College of Nursing</div>
-                </div>
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">The University of Alabama College of Nursing</div>
 
+                    </div>
                 </Link>
-
                 <Link to="https://aafa.org/"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
-                    <div className="logo-container text-center text-2xl p-2" style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}>
-                    <img src='/images/AAFA_logo.png' alt="/images/AAFA_logo.png" />
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/AAFA_logo.png' alt="/images/AAFA_logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
 
-                    <div>Asthma and Allergy Foundation of America</div>
-                </div>
-                </Link>
-                <Link to="https://acaai.org/asthma/asthma-101/who-gets-asthma/children/"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
-                    <div className="logo-container text-center text-2xl p-2" style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundImage: `url(${ACAAI_logo})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}></div>
-                    <div className='p-3'>American College of Allergy, Asthma, and Immunology</div>
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">Asthma and Allergy Foundation of America</div>
+
+                    </div>
                 </Link>
                 <Link to="https://www.mayoclinic.org/"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
-                    <div className="logo-container" style={{
-                        width: '100px',
-                        height: '100%',
-                        backgroundImage: `url(${mayo_logo})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}></div>
-                    <span className="mr-auto pl-2 ">The Mayo Clinic</span>
-                </Link>
-                <Link to="https://ginasthma.org/gina-patient-guide-you-can-control-your-asthma/"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/mayo_logo.png' alt="/images/mayo_logo.png" style={{
+                            maxHeight: '70%',
+                            maxWidth: 'auto'
+                            
+                        }} />
 
-                    <div className="logo-container text-center text-2xl p-2" style={{
-                        width: '500px',
-                        height: '100px',
-                        backgroundImage: `url(${GIA_logo})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}></div>
-                    <div className='p-3'>Helpful Patient Guide</div>
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">The Mayo Clinic</div>
+                    </div>
+                </Link>
+                <Link to="https://acaai.org/asthma/asthma-101/who-gets-asthma/children/"
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/ACAAI_logo.png' alt="/images/ACAAI_logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
+
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">American College of Allergy, Asthma, and Immunology</div>
+                    </div>
+                </Link>
+                
+                <Link to="https://ginasthma.org/gina-patient-guide-you-can-control-your-asthma/"
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/GIA_Logo.png' alt="/images/GIA_Logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
+
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">Helpful Patient Guide</div>
+                    </div>
                 </Link>
                 <Link to="https://www.aaaai.org/tools-for-the-public/conditions-library/asthma/childhood-asthma"
-                    className='border border-gray-300 rounded-lg flex items-center justify-center px-4 py-6 hover:bg-gray-50'>
+                    className='flex flex-wrap border border-gray-300 rounded-lg items-center justify-center px-4 py-6 hover:bg-gray-50' style={{ height: '100%' }}>
+                    <div className="logo-container text-center text-2xl p-2">
+                        <img className='pb-4' src='/images/AAAAI_logo.png' alt="/images/AAAAI_logo.png" style={{
+                            maxWidth: '100%',
+                            maxHeight: '100%'
+                        }} />
 
-                    <div className="logo-container text-center text-2xl p-2" style={{
-                        width: '100%',
-                        height: '100%',
-                        backgroundImage: `url(${GIA_logo})`,
-                        backgroundSize: 'contain',
-                        backgroundRepeat: 'no-repeat',
-                    }}></div>
-                    <div className='p-3'>Does my child have asthma?</div>
+                        <div className="text-center text-2xl p-2 border-t border-gray-300">Does my child have asthma?</div>
+                    </div>
                 </Link>
             </div>
         </div>
