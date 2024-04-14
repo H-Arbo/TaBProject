d40b75dfc33e31fd84bbf48211528aacce7b5e8f
Logan Shultz <lshultz@crimson.ua.edu>
2024-04-14 03:04:47 +0000

    app making in progress

--- mobile/tab-to-stop/app/_layout.tsx
+++ mobile/tab-to-stop/app/_layout.tsx
@@ -27,6 +27,7 @@ SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
   const [loaded, error] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    
     ...FontAwesome.font,
   });
 
@@ -50,11 +51,10 @@ export default function RootLayout() {
 
 function RootLayoutNav() {
   const colorScheme = useColorScheme();
-  // return <Text>Hello</Text>;
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "light" ? DarkTheme : DefaultTheme}>
       <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false,  }} />
         <Stack.Screen
           name="profile_menu"
           options={{
--- mobile/tab-to-stop/components/Themed.tsx
+++ mobile/tab-to-stop/components/Themed.tsx
@@ -6,7 +6,6 @@
 import { Text as DefaultText, View as DefaultView } from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from './useColorScheme';
 
 type ThemeProps = {
   lightColor?: string;
@@ -20,7 +19,7 @@ export function useThemeColor(
   props: { light?: string; dark?: string },
   colorName: keyof typeof Colors.light & keyof typeof Colors.dark
 ) {
-  const theme = useColorScheme() ?? 'light';
+  const theme = 'light'; // Force light theme
   const colorFromProps = props[theme];
 
   if (colorFromProps) {
