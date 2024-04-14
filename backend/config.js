8ebaccd4c3cbd539c6e35aa2686dbf0b8cf4d43d
Hayden Arbogast <harboga4@gmail.com>
2024-04-14 18:50:58 +0000

    port to 8080

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-export const PORT = 8081;
+export const PORT = 8080;
 
 export const mongoDBURL =
 'mongodb+srv://hgfairfax:yEPAu5r6MKCqyQtk@tab-to-stop.rrlhka4.mongodb.net/TaB-to-stop?retryWrites=true&w=majority';
