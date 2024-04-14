50e6de466fb047b175cb11a97cfd3bb62e9cfccb
Hayden Arbogast <harboga4@gmail.com>
2024-04-14 14:31:43 +0000

    port edits

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], server: {port: 8080},
+  plugins: [react()], server: {port: 5173},
 })
