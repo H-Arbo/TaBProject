ddb6ac067b5169ad1e45625874eade35201406a1
Hayden Arbogast <harboga4@gmail.com>
2024-04-15 14:25:04 +0000

    cors net

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const app = express();
 app.disable("x-powered-by");
 //middleware for parsing request body
 app.use(express.json());
-var allowedOrigins = ["http://localhost:8080", "http://localhost:5173"];
+var allowedOrigins = ["http://localhost:8080", "http://localhost:5173", "http://cs495-spring2024-01.ua.edu:8080"];
 //cors middleware
 app.use(
   cors({
