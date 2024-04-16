e0a470097c7c9b6bf33d7528d53212af49be7dbb
Hayden Arbogast <harboga4@gmail.com>
2024-04-16 19:45:35 +0000

    box color update

--- frontend/src/pages/PMeds.jsx
+++ frontend/src/pages/PMeds.jsx
@@ -274,7 +274,7 @@ const PMeds = () => {
             </table>
             <table className="w-full">
               <thead>
-                <tr className="bg-green-200">
+                <tr className="bg-yellow-200">
                   <th className="p-3 border-r border-yellow-600">Medication</th>
                   <th className="p-3 border-r border-yellow-600">Amount</th>
                   <th className="p-3 border-r border-yellow-600">Frequency</th>
