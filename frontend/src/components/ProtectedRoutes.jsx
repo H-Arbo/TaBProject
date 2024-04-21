0346f400b749fa387f9b6936536ece9bda1292b2
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-04-21 09:51:51 -0500

    context

--- frontend/src/components/ProtectedRoutes.jsx
+++ frontend/src/components/ProtectedRoutes.jsx
@@ -1,11 +1,12 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation} from "react-router-dom";
 import { UserContext } from "../../context/userContext";
 import { useContext } from 'react';
 
 export const ProtectedRoute = ({ children }) => {
   const { user } = useContext(UserContext);
+  const location = useLocation();
   console.log(user);
-  if (!user) {
+  if (!user && location.state) {
     // user is not authenticated
     return <Navigate to="/" />;
   }
