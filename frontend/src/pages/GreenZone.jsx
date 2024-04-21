29ac85ab0045279669964ed02ddddd8d478d8bc8
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-04-21 13:14:58 -0500

    green zone no med response

--- frontend/src/pages/GreenZone.jsx
+++ frontend/src/pages/GreenZone.jsx
@@ -2,14 +2,14 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Loading from "../components/Loading";
 import Pat_Navbar from "../components/Pat_Navbar";
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation } from "react-router-dom";
 
 const GreenZone = () => {
   const [patient, setPatient] = useState(null);
   const [loading, setLoading] = useState(false);
   const [editMode, setEditMode] = useState(false);
   const location = useLocation();
-  const { pat_email , doctor_email} = location.state;
+  const { pat_email, doctor_email } = location.state;
   const changeToFalse = () => {
     setEditMode(false);
   };
@@ -30,14 +30,14 @@ const GreenZone = () => {
 
   return (
     <>
-      <Pat_Navbar email={pat_email} doctor_email ={ doctor_email}/>
+      <Pat_Navbar email={pat_email} doctor_email={doctor_email} />
       <div className="p-9 bg-white">
         {loading ? (
           <Loading />
         ) : patient ? (
           <div className="flex justify-center items-center">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              {patient.gz_meds && patient.gz_meds.length > 0 && (
+              {patient.gz_meds && patient.gz_meds.length > 0 ? (
                 <div className="border border-green-600 rounded-md">
                   <h2 className="bg-green-200 text-gray-800 text-lg font-bold p-3 text-center">
                     Green Zone
@@ -84,6 +84,15 @@ const GreenZone = () => {
                       ))}
                     </tbody>
                   </table>
+                </div>
+              ) : (
+                <div className="p-2">
+                  <h2 className="text-center ">
+                    <strong>No Green Zone Medication</strong>
+                  </h2>
+                  <p className="text-center text-gray-600">
+                    If this is a mistake, contact your care provider.
+                  </p>
                 </div>
               )}
             </div>
