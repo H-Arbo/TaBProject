802eb0d4b97f9f07dc20e3c3bccd44ffbdc4f34b
Hayden Arbogast <htarbogast@crimson.ua.edu>
2024-04-21 13:45:59 -0500

    editPatient logging

--- backend/controllers/pAuthControllers.js
+++ backend/controllers/pAuthControllers.js
@@ -549,6 +549,7 @@ export const editPatient = async (request, response) => {
   const containsCapitalRegex = /[A-Z]/;
   const containsEmojiRegex = /[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/u;
   try {
+    console.log(request.body);
     const {
       name,
       age,
